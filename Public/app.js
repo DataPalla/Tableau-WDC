1dcb90a951d653515c70cf973e2f1776b9794566
Datapalla <sandeepsai498@yahoo.com>
2020-11-16 19:01:02 -0600

    update

--- Public/app.js
+++ Public/app.js
@@ -115,24 +115,33 @@ function readCSVFile(response) {
   for (var i = 0; i < lines.length; i++) {
      var _firstColumn = lines[i].split(",");     //First column (Split on the separator!)
      //Do your stuff
+     for( var j =0; j < _firstColumn.length; j++ ){
+      if(_firstColumn[i].includes("") != "true"){
+        _firstColumn[i] = _firstColumn[0].replace(/\"/g, "");
+      }
+     }
+     if(_firstColumn[28].includes("") != true){
+      _firstColumn[28] = _firstColumn["28"].substring(6).replace(/\"/g, "").trim();
+     }
+
       tableData.push({
-        provider_id: _firstColumn[0].replace(/\"/g, ""),
-        hospital_name: _firstColumn[1].replace(/\"/g, ""),
-        address: _firstColumn[2].replace(/\"/g, ""),
-        city: _firstColumn[3].replace(/\"/g, ""),
-        state: _firstColumn[4].replace(/\"/g, ""),
-        zip_code: _firstColumn[5].replace(/\"/g, ""),
-        county_name: _firstColumn[6].replace(/\"/g, ""),
-        phone_number: _firstColumn[7].replace(/\"/g, ""),
-        hospital_type: _firstColumn[8].replace(/\"/g, ""),
-        hospital_ownership: _firstColumn[9].replace(/\"/g, ""),
-        emergency_services: _firstColumn[10].replace(/\"/g, ""),
-        meets_criteria_for_meaningful_use_of_ehrs: _firstColumn[11].replace(/\"/g, ""),
-        hospital_overall_rating: _firstColumn[12].replace(/\"/g, ""),
-        hospital_overall_rating_footnote: _firstColumn[13].replace(/\"/g, ""),
-        mortality_national_comparison: _firstColumn[14].replace(/\"/g, ""),
-        mortality_national_comparison_footnote: _firstColumn[15].replace(/\"/g, ""),
-        geocoded_column: _firstColumn["28"].substring(6).replace(/\"/g, "").trim(),
+        provider_id:_firstColumn[0],
+        hospital_name: _firstColumn[1],
+        address: _firstColumn[2],
+        city: _firstColumn[3],
+        state: _firstColumn[4],
+        zip_code: _firstColumn[5],
+        county_name: _firstColumn[6],
+        phone_number: _firstColumn[7],
+        hospital_type: _firstColumn[8],
+        hospital_ownership: _firstColumn[9],
+        emergency_services: _firstColumn[10],
+        meets_criteria_for_meaningful_use_of_ehrs: _firstColumn[11],
+        hospital_overall_rating: _firstColumn[12],
+        hospital_overall_rating_footnote: _firstColumn[13],
+        mortality_national_comparison: _firstColumn[14],
+        mortality_national_comparison_footnote: _firstColumn[15],
+        geocoded_column: _firstColumn[28],
      });  
   }
   
