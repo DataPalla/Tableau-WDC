dd894a2a5f920ca6da51b674a5d4899d71eea163
Datapalla <sandeepsai498@yahoo.com>
2020-11-16 20:27:45 -0600

    update

--- Public/app1.js
+++ Public/app1.js
@@ -73,7 +73,7 @@ console.log("This is working!");
       },
       {
         id: "geocoded_column",
-        dataType: tableau.dataTypeEnum.int,
+        dataType: tableau.dataTypeEnum.string,
       },
     ];
 
@@ -87,38 +87,15 @@ console.log("This is working!");
   };
 
   myConnector.getData = function (table, doneCallback) {
-    let tableData = [];
-    var i = 0;
-
-    $.getJSON(
-      "https://data.medicare.gov/resource/xubh-q36u.json",
-      function (resp) {
-        // Iterate over the JSON object
-        for (i = 0, len = resp.length; i < len; i++) {
-          tableData.push({
-            provider_id: resp[i].provider_id,
-            hospital_name: resp[i].hospital_name,
-            address: resp[i].address,
-            city: resp[i].city,
-            state: resp[i].state,
-            zip_code: resp[i].zip_code,
-            county_name: resp[i].county_name,
-            phone_number: resp[i].phone_number,
-            hospital_type: resp[i].hospital_type,
-            hospital_ownership: resp[i].hospital_ownership,
-            emergency_services: resp[i].emergency_services,
-            meets_criteria_for_meaningful_use_of_ehrs: resp[i].meets_criteria_for_meaningful_use_of_ehrs,
-            hospital_overall_rating: resp[i].hospital_overall_rating,
-            hospital_overall_rating_footnote: resp[i].hospital_overall_rating_footnote,
-            mortality_national_comparison: resp[i].mortality_national_comparison,
-            mortality_national_comparison_footnote: resp[i].mortality_national_comparison_footnote,
-            geocoded_column: resp[i].geocoded_column,
-         });
-        }
-        table.appendRows(tableData);
+    $.ajax({
+      url:"https://data.medicare.gov/resource/xubh-q36u.csv",
+      success: function (resp) {
+        td = readCSVFile(resp);
+        table.appendRows(td);
         doneCallback();
-      }
-    );
+        }
+      });
+      
   };
 
   tableau.registerConnector(myConnector);
@@ -130,3 +107,43 @@ function getData() {
   tableau.connectionName = "Hospital Compare Data";
   tableau.submit();
 }
+
+function readCSVFile(response) {
+  var table;
+  var lines = response.split("\n");
+  let tableData = [];
+  for (var i = 0; i < lines.length; i++) {
+     var _firstColumn = lines[i].split(",");     //First column (Split on the separator!)
+     //Do your stuff
+     for( var j =0; j < _firstColumn.length; j++ ){
+      if((_firstColumn[j].includes("",1) != 0) || (_firstColumn[j]!== undefined)){
+        _firstColumn[j] = _firstColumn[j].replace(/\"/g, "");
+      }
+     }
+     if((_firstColumn[28].includes("",1) != 0) || (_firstColumn[j]!== undefined)){
+      _firstColumn[28] = _firstColumn["28"].substring(6).replace(/\"/g, "").trim();
+     }
+
+      tableData.push({
+        provider_id:_firstColumn[0],
+        hospital_name: _firstColumn[1],
+        address: _firstColumn[2],
+        city: _firstColumn[3],
+        state: _firstColumn[4],
+        zip_code: _firstColumn[5],
+        county_name: _firstColumn[6],
+        phone_number: _firstColumn[7],
+        hospital_type: _firstColumn[8],
+        hospital_ownership: _firstColumn[9],
+        emergency_services: _firstColumn[10],
+        meets_criteria_for_meaningful_use_of_ehrs: _firstColumn[11],
+        hospital_overall_rating: _firstColumn[12],
+        hospital_overall_rating_footnote: _firstColumn[13],
+        mortality_national_comparison: _firstColumn[14],
+        mortality_national_comparison_footnote: _firstColumn[15],
+        geocoded_column: _firstColumn[28],
+     });  
+  }
+  
+  return tableData;
+};
